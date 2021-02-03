fc80ffbb93e1420866bf3fd06cc9e04febcc2b44
jpradass <josue.pradas@hotmail.com>
2021-02-03 16:12:55 +0100

    update

--- code/resources/recipe.py
+++ code/resources/recipe.py
@@ -3,20 +3,6 @@ from flask_restful import Resource, reqparse
 from models.recipe import RecipeModel
 
 class Recipe(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument(
-        "name", type=str, required=True, help="This field cannot be left blank", location='json'
-    )
-    parser.add_argument(
-        "calories", type=float, required=True, help="Every recipe has its calories", location='json'
-    )
-    parser.add_argument(
-        "instructions", type=str, required=True, help="Every recipe needs its instructions", location='json'
-    )
-    parser.add_argument(
-        "ingredients", type=list, required=False, help="Every recipe has a way to be done", location='json'
-    )
-
     @jwt_required
     def get(self, uuid):
         recipe = RecipeModel.findby_id(uuid)
@@ -26,10 +12,24 @@ class Recipe(Resource):
 
     @jwt_required
     def post(self, uuid):
+        parser = reqparse.RequestParser()
+        parser.add_argument(
+            "name", type=str, required=True, help="This field cannot be left blank", location='json'
+        )
+        parser.add_argument(
+            "calories", type=float, required=True, help="Every recipe has its calories", location='json'
+        )
+        parser.add_argument(
+            "instructions", type=str, required=True, help="Every recipe needs its instructions", location='json'
+        )
+        parser.add_argument(
+            "ingredients", type=list, required=True, help="Every recipe has a way to be done", location='json'
+        )
+
         if RecipeModel.findby_id(uuid):
             return {'message': "Recipe with this uuid already exists", 'description': uuid}, 400
         
-        data = Recipe.parser.parse_args()
+        data = parser.parse_args()
         recipe = RecipeModel(uuid, **data)
 
         recipe.saveto_db()
@@ -37,11 +37,25 @@ class Recipe(Resource):
 
     @jwt_required
     def put(self, uuid):
+        parser = reqparse.RequestParser()
+        parser.add_argument(
+            "name", type=str, required=False, help="This field cannot be left blank", location='json'
+        )
+        parser.add_argument(
+            "calories", type=float, required=False, help="Every recipe has its calories", location='json'
+        )
+        parser.add_argument(
+            "instructions", type=str, required=False, help="Every recipe needs its instructions", location='json'
+        )
+        parser.add_argument(
+            "ingredients", type=list, required=False, help="Every recipe has a way to be done", location='json'
+        )
+
         recipe = RecipeModel.findby_id(uuid)
         if not recipe:
             return {'message': 'Recipe not found', 'description': uuid}, 404
 
-        data = Recipe.parser.parse_args()
+        data = parser.parse_args()
         if data['name'] is not None:
             recipe.name = data['name']
         if data['calories'] is not None:
